0327fe6723caac5ef91da8a6c998e0c5af54d603
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-07 23:44:39 +0530

    Commit Appijs filse

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
-// content of index.js
-process.stdout.write('\u001B[2J\u001B[0;0f');
-const http = require('net')
-const port = 3000
+var http = require('http');
 
-//const requestHandler = (request, response) => {
-//  console.log(request.url)
-//  response.end('Hello Node.js Server!')
-//}
-var iCounter = 1;
-let sockets = {};
-const server = http.createServer()
-
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening on ${port}`)
+var server = http.createServer(function(req, res) {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
 });
-server.on('connection', (socket) => {
-    socket.id = iCounter++;
-    sockets[socket.id] = socket;
-    console.log('Client Connected');
-    socket.write('Welecom new client!\r\n');
-
-    socket.on('data', data=> {
-        //console.log(socket);
-        for (var prop in sockets) {
-            var cs = sockets[prop];
-            cs.write(socket.id + ': ' + data + '\n\r');
-        }
-        //Object.entries(sockets).foreach((cs)=>{
-        //    cs.write(socket.id + ': ' + data + '\n\r');
-        //});
-        
-    });
-    socket.on('end', () => {
-        console.log('Client is disconnected now');
-    });
-});
+server.listen(8080);
