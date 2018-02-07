b9031f0dc3f00f9e4fa5c8d8ce692311bd46d0cb
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-08 00:51:46 +0530

    commit

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
-res.writeHead(200, {"Content-Type": "text/html"});
-res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
-});
-server.listen(8080);
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+    
+}).listen(process.env.PORT || 8080);
