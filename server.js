7a232efcdd6ebf9f3a195d52cc89aad75ccf6191
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-13 03:47:02 +0530

    hkh

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 var express=require('express');
 var app=express();
 var appBaseUrl="/api";
-
+var port=process.env.PORT|| 8080;
 app.get(appBaseUrl+"/",function(req,res){	
 	res.send("welcome to my dream app api");
 });
 
-app.listen("8080",function(){
-	console.log('running on port :'+"8080"); 	
+app.listen(port,function(){
+	console.log('running on port :'+port); 	
 });
 
