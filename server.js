d145e2732db4e2074fc3a909d7b1ce3cd9654b9d
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-13 04:24:13 +0530

    code change

--- server.js
+++ server.js
@@ -7,23 +7,12 @@ var port = process.env.PORT|| 8080;
 //	res.send("welcome to my dream app api");
 //});
 //
-//app.listen(port,function(){
-//	console.log('running on port :'+port); 	
-//});
-//
+app.listen(port,function(){
+console.log('running on port :'+port); 	
+});
 
-var http = require("http");
 
-http.createServer(function (request, response) {
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(port);
 
 
 
