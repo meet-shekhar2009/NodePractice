732cf2a1c362c59a54f262fdb54986d075924cf3
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-13 03:58:14 +0530

    jhjh

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
 var express=require('express');
-var app=express();
-var appBaseUrl="/api";
-var port=process.env.PORT|| 8080;
+var app = express();
+var appBaseUrl = "/api";
+var port = process.env.PORT|| 8080;
 app.get(appBaseUrl+"/",function(req,res){	
 	res.send("welcome to my dream app api");
 });
