ad0d48b529ed2675af64de414bbef9fdf19bbaf3
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-13 02:10:11 +0530

    commit:Json result by chandra shekhar

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var app=http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end({Name:'Chandra Shekhar',Age:29});
+    res.end(JSON.stringify({Name:'Chandra Shekhar',Age:29}));
     
 }).listen(process.env.PORT || 8080);
 
