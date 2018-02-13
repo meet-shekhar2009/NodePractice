eea1e66e7fa3f4c0f64ef787de93580fa0f2fc8e
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-13 15:09:59 +0530

    Commit:CHandra

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express=require('express');
 var app = express();
 var appBaseUrl = "/api";
 var port = process.env.PORT|| 8080;
-//app.get(appBaseUrl+"/",function(req,res){	
-//	res.send("welcome to my dream app api");
-//});
-//
+app.get(appBaseUrl+"/",function(req,res){	
+	res.send("<h2>welcome to my dream app api</h2>");
+});
+
 app.listen(port,function(){
 console.log('running on port :'+port); 	
 });
