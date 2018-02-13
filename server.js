037a9e0f9cecea52e752240a6863afcd0dead6f4
mmet-shekhar2009 <cshekhar@adobe.com>
2018-02-14 00:09:47 +0530

    chat socketio

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 
-var express=require('express');
-var app = express();
-var appBaseUrl = "/api";
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var cors = require('cors');
+app.use(cors());
+
+
+var appBaseUrl = "/";
 var port = process.env.PORT|| 8080;
 app.get(appBaseUrl+"/",function(req,res){	
-	res.send("<h2>welcome to my dream app api</h2>");
+	res.sendFile(__dirname+'/index.html');
 });
 
-app.listen(port,function(){
+server.listen(port,function(){
 console.log('running on port :'+port); 	
 });
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('hello', function(data){
+  io.sockets.emit('hello',data);
+});
+});
+
 
 
 
